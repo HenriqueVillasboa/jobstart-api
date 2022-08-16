0543f30992607bb341edea165fd5d0ed948bad67
HenriqueVillasboa <villasboahenrique@gmail.com>
2022-08-16 19:18:50 -0300

    Adiciona metodo addResourceHandlers na classe WebConfig

--- src/main/java/br/com/unifil/jobstartapi/config/WebConfig.java
+++ src/main/java/br/com/unifil/jobstartapi/config/WebConfig.java
@@ -1,11 +1,14 @@
 package br.com.unifil.jobstartapi.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan("br.com.unifil.jobstartapi")
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
@@ -13,5 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }
